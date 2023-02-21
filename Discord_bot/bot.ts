9645a62cf6ec9e8b8e4efc988f1e344b3c74f920
Andreas Johansson <andreasjohansson@student-236-71.eduroam.uu.se>
2023-02-21 12:31:15 +0100

    Fösta commit

---
+++ Discord_bot/bot.ts
@@ -0,0 +1,43 @@
+import { Pair } from "../../../lib/list";
+
+const { REST, Routes } : any = require('discord.js');
+
+type commands = Pair<String, String>
+
+const commands  = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest: any = new REST({ version: '10' }).setToken('MTA3NTczMTk0OTEyNzIwODk4Nw.GlD_MP.7NWWQfWkZ3dt2Pf3NEfzVxP-KvU6xyIBCaiVrA');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands('1075731949127208987'), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('MTA3NTczMTk0OTEyNzIwODk4Nw.GlD_MP.7NWWQfWkZ3dt2Pf3NEfzVxP-KvU6xyIBCaiVrA');
--- Evenemags_Guiden.ts
+++ Evenemags_Guiden.ts
@@ -1,2 +1,11 @@
+
+
 //Första filen
+
+//Selenuim Drivers
+const {Builder} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+const service = new chrome.ServiceBuilder('/chromedriver');
+const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
 
