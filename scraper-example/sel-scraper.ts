f0fcf79f3adfdac81811a4e349fa0f821a6db7f7
josiasRoe <josias.roeland@gmail.com>
2023-03-03 16:43:38 +0100

    scraper funkar

--- scraper-example/sel-scraper.ts
+++ scraper-example/sel-scraper.ts
@@ -40,21 +40,33 @@ async function scrapeSite(url: string, filename: string) {
         const button1 = await driver.findElement(By.css("#datepicker-9018"));//#datepicker-9018
         await button1.click();
 
-        const button2 = await driver.findElement(By.css("#datepicker-conatainer-9018 > div > div.filter-actions.filter-actions-ranges > div > ul > li:nth-child(3)"));//#datepicker-conatainer-9018 > div > div.filter-actions.filter-actions-ranges > div > ul > li:nth-child(3)
+        // const button2 = await driver.findElement(By.css("#datepicker-conatainer-9018 > div > div.filter-actions.filter-actions-ranges > div > ul > li:nth-child(3)"));//#datepicker-conatainer-9018 > div > div.filter-actions.filter-actions-ranges > div > ul > li:nth-child(3)
+        // await button2.click();
+        
+        const button2 = await driver.findElement(By.css("#datepicker-conatainer-9018 > div > div.filter-options > div.drp-calendar.left > div.calendar-table > table > tbody > tr > td.today.active.start-date.available"));
         await button2.click();
+        const button3 = await driver.findElement(By.css("#datepicker-conatainer-9018 > div > div.filter-options > div.drp-calendar.right > div.calendar-table > table > tbody > tr:nth-child(6) > td:nth-child(8)"));
+        await button3.click();
+        const button4 = await driver.findElement(By.css("#datepicker-conatainer-9018 > div > div.drp-buttons.filter-footer > div > div.btn-group.do-filter > button"));
+        await button4.click();
 
         // Wait for the page to load after the button click
-        driver.wait(until.urlContains("page-loaded-url"), 10000);
+        //await driver.wait(until.urlContains("page-loaded-url"), 10000);
+        await driver.sleep(10000);
+
+        const dateScrape = await driver.findElement(By.xpath('//*[@id="datepicker-9018"]'));
+        console.log(dateScrape);
+        
 
         // Scrape the data from the page
-        const scrapedData = await driver.findElement(By.css("#event-collection-9018 > div > ul > li.event-category.event-color-5.expanded")).getText();
-        //const scrapedData = await driver.findElement(By.css("#event-collection-9018 > div > ul > li.event-category.event-color-11.expanded")).getText();
 
-        
+        //const scrapedData = await driver.findElement(By.css("#event-collection-9018 > div > ul > li.event-category.event-color-5.expanded")).getText();
+        //const scrapedData = await driver.findElement(By.css("#event-collection-9018 > div > ul > li.event-category.event-color-11.expanded")).getText();
+        const scrapedData = await driver.findElement(By.xpath('//*[@id="event-collection-9018"]/div/ul/li[10]'));
         console.log(scrapedData);
         
 
-        saveData(scrapedData, filename);
+        saveData(await scrapedData.getText(), filename);
 
     } finally {
         await driver.quit();
