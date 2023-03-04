e60915afb6eab960a5099fba2d8838a31580930b
josiasRoe <josias.roeland@gmail.com>
2023-03-04 20:30:50 +0100

    kan nu scrapea alla element var för sig och sparar de som arrayer i en array

--- scraper-example/sel-scraper.ts
+++ scraper-example/sel-scraper.ts
@@ -3,25 +3,46 @@ import { existsSync, mkdirSync, writeFile } from "fs";
 import { Builder, By, Key, until } from "selenium-webdriver";
 import { Options } from "selenium-webdriver/chrome";
 
+function saveData(Data: string, filename: string): void{
+
+    if (!existsSync('data')) {
+        mkdirSync('data');
+    }
+    writeFile(
+        'data/'+ filename, 
+        Data, 
+        err => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("File created!");
+        });
+}
+
+async function getInfo(url: string){
+    const options = new Options();
+    options.addArguments("--headless"); // Run Chrome in headless mode
+    options.addArguments("--disable-blink-features=AutomationControlled");
+
+    const driver = await new Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(options)
+        .build();
+    try {
+        await driver.get(url);
+        await driver.wait(until.urlContains("evid="), 10000);
+        const info =  await driver.findElement(By.xpath('//*[@id="wap-section-9019"]/div[2]/div/div[1]/div[2]'))
+        
+        return await info.getText();
+
+    } finally {
+        await driver.quit();
+    }
+}
 
 async function scrapeSite(url: string, filename: string) {
 
-    function saveData(Data: string, filename: string){
-
-        if (!existsSync('data')) {
-            mkdirSync('data');
-        }
-        writeFile(
-            'data/'+ filename, 
-            Data, 
-            err => {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log("File created!");
-            });
-    }
-
+    
     const options = new Options();
     options.addArguments("--headless"); // Run Chrome in headless mode
     options.addArguments("--disable-blink-features=AutomationControlled");
@@ -51,23 +72,43 @@ async function scrapeSite(url: string, filename: string) {
         await button4.click();
 
         // Wait for the page to load after the button click
-        //await driver.wait(until.urlContains("page-loaded-url"), 10000);
+        
         await driver.sleep(10000);
 
-        const dateScrape = await driver.findElement(By.xpath('//*[@id="datepicker-9018"]'));
-        console.log(dateScrape);
+        //const dateScrape = await driver.findElement(By.xpath('//*[@id="event-category-11"]'));
+        //console.log(dateScrape);
         
 
         // Scrape the data from the page
 
         //const scrapedData = await driver.findElement(By.css("#event-collection-9018 > div > ul > li.event-category.event-color-5.expanded")).getText();
         //const scrapedData = await driver.findElement(By.css("#event-collection-9018 > div > ul > li.event-category.event-color-11.expanded")).getText();
-        const scrapedData = await driver.findElement(By.xpath('//*[@id="event-collection-9018"]/div/ul/li[10]'));
-        console.log(scrapedData);
+        //const scrapedData = await driver.findElement(By.xpath('//*[@id="event-category-11"]'));
+        //console.log(scrapedData);
+       
+        const links = await driver.findElements(By.xpath('//*[@id="event-category-11"]/li'));
+        //console.log(await links.forEach(link => link.getText()));
+        console.log(links.length);
+        
+        const allInfo: any[] = [];
+
+        for(let i = 1 ; i <= links.length; i++){
+            const hrefElement = await driver.findElement(By.xpath('//*[@id="event-category-11"]/li[' + i + ']/div/a'));
+            const href = await hrefElement.getAttribute("href");
+            console.log(href);
+
+            const info = await getInfo(href);
+
+            console.log(info);
+            const infoArr: string[] = [info];
+            allInfo.push(infoArr);
+        }
+
+        console.log(allInfo);
         
 
-        saveData(await scrapedData.getText(), filename);
-
+        //saveData(await links[0].getText(), filename);
+        saveData(JSON.stringify(allInfo), filename);    
     } finally {
         await driver.quit();
     }
